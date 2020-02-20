4c1c9b91212bd5e2b19a8f3e1ea3d677b13e5aba
9ntonio <aalmena@gmail.com>
2020-02-20 15:14:33 -0800

    updating 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,7 +9,7 @@ export default () => (
     <Helmet title="Oh Boy!" />
 
     <Intro>
-      <h1>Hello 👋🏽 It looks like the page you are looking for was not found.</h1>
+      <h1>Oh Boy❗That didn't go as planned!⛔</h1>
     </Intro>
 
     <Video to="https://vimeo.com/54784090?playsinline=0" link="https://www.cnn.com/travel/article/paris-best-time-to-visit-spring/index.html" header="While you're here, enjoy Paris in in spring." />
